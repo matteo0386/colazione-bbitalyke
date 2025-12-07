31e92b192d5cfff85d304ce48841de9b0296ecf8
matteo0386 <matteo0386@outlook.it>
2025-12-07 22:59:42 +0100

    Create config-public.js

---
+++ config-public.js
@@ -0,0 +1,7 @@
+// CONFIG PUBBLICA (NON contiene token)
+
+const OWNER = "matteo0386";
+const REPO = "bbitalyke-orders";
+
+// Endpoint della GitHub Action (te lo fornisco tra poco)
+const ORDER_ENDPOINT = "https://api.github.com/repos/matteo0386/bbitalyke-orders/dispatches";
